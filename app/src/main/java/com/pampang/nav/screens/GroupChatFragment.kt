ed48f3d05831da354f219b4351a7fb8ffcb9ebb0
Don <kalixjames001@gmail.com>
2025-11-05 22:42:04 +0800

    Initial Commit

--- app/src/main/java/com/pampang/nav/screens/GroupChatFragment.kt
+++ app/src/main/java/com/pampang/nav/screens/GroupChatFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -11,6 +12,7 @@ import com.pampang.nav.adapters.GroupChatAdapter
 import com.pampang.nav.databinding.FragmentGroupChatBinding
 import com.pampang.nav.viewmodels.GroupChatViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -43,6 +45,12 @@ class GroupChatFragment : Fragment() {
                 if (it.isNotEmpty()) {
                     binding.recyclerViewChat.scrollToPosition(it.size - 1)
                 }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.networkError.collectLatest {
+                Toast.makeText(requireContext(), "Network is Unstable", Toast.LENGTH_SHORT).show()
             }
         }
 
--- app/src/main/java/com/pampang/nav/viewmodels/GroupChatViewModel.kt
+++ app/src/main/java/com/pampang/nav/viewmodels/GroupChatViewModel.kt
@@ -1,25 +1,39 @@
 package com.pampang.nav.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pampang.nav.repositories.GroupChatRepository
+import com.pampang.nav.utilities.NetworkUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GroupChatViewModel @Inject constructor(
-    private val groupChatRepository: GroupChatRepository
+    private val groupChatRepository: GroupChatRepository,
+    private val application: Application
 ) : ViewModel() {
 
     val messages = groupChatRepository.getGroupChatMessages()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 
+    private val _networkError = MutableSharedFlow<Unit>()
+    val networkError = _networkError.asSharedFlow()
+
     fun sendMessage(text: String) {
-        viewModelScope.launch {
-            groupChatRepository.sendGroupChatMessage(text)
+        if (NetworkUtils.isNetworkAvailable(application)) {
+            viewModelScope.launch {
+                groupChatRepository.sendGroupChatMessage(text)
+            }
+        } else {
+            viewModelScope.launch {
+                _networkError.emit(Unit)
+            }
         }
     }
 }
