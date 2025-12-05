f9df6cfd61f44c9a63085b21694a1f821ede54c5
Don <kalixjames001@gmail.com>
2025-12-05 19:22:57 +0800

    Initial Commit

--- app/src/main/java/com/pampang/nav/MapNav/NodeDisplayNames.java
+++ app/src/main/java/com/pampang/nav/MapNav/NodeDisplayNames.java
@@ -19,6 +19,100 @@ public class NodeDisplayNames {
             return "RightWing";
         } else if ("n48".equals(nodeLabel)) {
             return "Pampang Office";
+        } else if ("n4".equals(nodeLabel)) {
+            return "MOD - RIZAL VEGETABLES AND FLOWER SHOP";
+        } else if ("n53".equals(nodeLabel)) {
+            return "PALENGKE HERO";
+        } else if ("n9".equals(nodeLabel)) {
+            return "BGM \"BABY\" FARM PRODUCTS";
+        } else if ("n10".equals(nodeLabel)) {
+            return "BOY & SITA";
+        } else if ("n13".equals(nodeLabel)) {
+            return "JOEY & QUEY BAGUIO VEGETABLE WHILESALER & RETAILER";
+        } else if ("n14".equals(nodeLabel)) {
+            return "JAENA & VEGETABLES STORE";
+        } else if ("n16".equals(nodeLabel)) {
+            return "JAY & REAH STORE";
+        } else if ("n92".equals(nodeLabel)) {
+            return "SDR BAGUIO VEGETABLES DEALER";
+        } else if ("n19".equals(nodeLabel)) {
+            return "VANGIE STORE";
+        } else if ("n20".equals(nodeLabel)) {
+            return "SARAH DRIED FISH STORE";
+        } else if ("n22".equals(nodeLabel)) {
+            return "RYAN AND APPLE KOREAN ITEMS";
+        } else if ("n23".equals(nodeLabel)) {
+            return "ZHEN & GWEN";
+        } else if ("n25".equals(nodeLabel)) {
+            return "RAMON & BETH FRUIT STORE";
+        } else if ("n26".equals(nodeLabel)) {
+            return "EDMON BARRIO VEGETABLES DEALER";
+        } else if ("n30".equals(nodeLabel)) {
+            return "ALEX & CINDY COCONUT STORE";
+        } else if ("n31".equals(nodeLabel)) {
+            return "STOCK ROOM";
+        } else if ("n32".equals(nodeLabel)) {
+            return "MARIBEL'S VEGETABLE STORE";
+        } else if ("n33".equals(nodeLabel)) {
+            return "FERDIE & BING BANANA & COCONUT STORE";
+        } else if ("n34".equals(nodeLabel)) {
+            return "ALING APING VEGETABLES STORE";
+        } else if ("n35".equals(nodeLabel)) {
+            return "RAMON & BETH STORE";
+        } else if ("n36".equals(nodeLabel)) {
+            return "GINA'S VEGETABLE STORE";
+        } else if ("n37".equals(nodeLabel)) {
+            return "PENG & LENG FRUITS & VEGETABLES STORE";
+        } else if ("n38".equals(nodeLabel)) {
+            return "BEN & LOLET (JERR) FROZEN FOODS";
+        } else if ("n39".equals(nodeLabel)) {
+            return "FELIX & EVELYN VEGETABLE DEALER & RETAILER";
+        } else if ("n40".equals(nodeLabel)) {
+            return "BEN & LOLET";
+        } else if ("n41".equals(nodeLabel)) {
+            return "MALOU FROZEN FOOD";
+        } else if ("n46".equals(nodeLabel)) {
+            return "TORING FROZEN FOODS";
+        } else if ("n47".equals(nodeLabel)) {
+            return "MARIO & REBING FOOD PRODUCTS";
+        } else if ("n49".equals(nodeLabel)) {
+            return "TESS GARCIA VEGETABLE DEALER";
+        } else if ("n50".equals(nodeLabel)) {
+            return "GINA'S STORE";
+        } else if ("n51".equals(nodeLabel)) {
+            return "RJ VEGETABLES STORE";
+        } else if ("n52".equals(nodeLabel)) {
+            return "JULIUS D GREAT STORE";
+        } else if ("n5".equals(nodeLabel)) {
+            return "NYONG'S SARI SARI STORE";
+        } else if ("n54".equals(nodeLabel)) {
+            return "CESAR & SHAI FOOD PRODUCTS";
+        } else if ("n55".equals(nodeLabel)) {
+            return "DIANA'S MEAT STORE";
+        } else if ("n56".equals(nodeLabel)) {
+            return "MILA SICAT";
+        } else if ("n57".equals(nodeLabel)) {
+            return "JEFF & JERZELL";
+        } else if ("n58".equals(nodeLabel)) {
+            return "WENDY";
+        } else if ("n59".equals(nodeLabel)) {
+            return "BENG PANTIG";
+        } else if ("n60".equals(nodeLabel)) {
+            return "CORA ROQUE";
+        } else if ("n61".equals(nodeLabel)) {
+            return "Alex and Cindy COCONUT STORE";
+        } else if ("n88".equals(nodeLabel)) {
+            return "JASON & FRIGA";
+        } else if ("n63".equals(nodeLabel)) {
+            return "Brod. Long Hair (Live tilapia and Fresh Bangus)";
+        } else if ("n70".equals(nodeLabel)) {
+            return "Bert and Ester Meat Store";
+        } else if ("n72".equals(nodeLabel)) {
+            return "Chona's Chicken Store";
+        } else if ("n76".equals(nodeLabel)) {
+            return "Brian and & Shara";
+        } else if ("n90".equals(nodeLabel)) {
+            return "Edgar and Ason";
         }
         // Add more mappings here for other stores
         return nodeLabel;
